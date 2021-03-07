8842db7d490a6f6990f57a98a98a93c5116fbc76
Isaaq Khader <isaaqkhader@uchicago.edu>
2021-03-07 03:28:21 +0000

    Change /messages to /anime

--- portfolio/src/main/java/com/google/sps/servlets/MessagesServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MessagesServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/messages")
+@WebServlet("/anime")
 public final class MessagesServlet extends HttpServlet {
     
     public class Anime {
